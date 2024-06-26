91f559cd6d10ad57ced97ce4e7a2ad58a7da71be
Jeff MacInnes <jeff.macinnes@gmail.com>
2024-06-25 21:46:06 -0700

    added Curtain

---
+++ src/lib/components/Helpers/Curtain.svelte
@@ -0,0 +1,16 @@
+<script>
+	export let opacity = 0.8;
+</script>
+
+<div class="curtain" style:background-color={`rgba(126, 126, 126, ${opacity})`} />
+
+<style lang="scss">
+	.curtain {
+		position: absolute;
+		top: 0;
+		left: 0;
+		width: 100%;
+		height: 100%;
+		backdrop-filter: blur(3px);
+	}
+</style>
