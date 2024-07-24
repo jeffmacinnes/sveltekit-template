5c478da62a08f9c77286659bc081afdec4444e55
Jeff MacInnes <jeff.macinnes@gmail.com>
2024-07-23 21:04:31 -0700

    updating doc on +page.js

--- src/routes/+page.js
+++ src/routes/+page.js
@@ -1,3 +1,6 @@
-// since there's no dynamic data here, we can prerender
-// it so that it gets served as a static asset in production
+/* since there's no dynamic data here, we can prerender  it so that it 
+gets served as a static asset in production.
+
+Also, if there is a +page.server.js file that is reading data from the file system, 
+pre-render must also be set to true */
 export const prerender = true;
