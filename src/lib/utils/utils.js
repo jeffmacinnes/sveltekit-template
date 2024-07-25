995f76c911b8539bf62057b372d4ad42f05e87fe
Jeff MacInnes <jeff.macinnes@gmail.com>
2024-07-25 13:20:21 -0700

    updated utils functions

--- src/lib/utils/utils.js
+++ src/lib/utils/utils.js
@@ -1,5 +1,67 @@
-// *** General math functions to make certain, frequently used calculations easier for us
+// === ARRAY FUNCTIONS ============================================================
+// return a value from an object at the given path (i.e. 'a.b.c' => obj[a][b][c])
+export const getNestedProperty = (obj, path) => {
+	const properties = path.split('.');
+	return properties.reduce((acc, key) => {
+		if (acc && acc.hasOwnProperty(key)) {
+			return acc[key];
+		} else {
+			return null;
+		}
+	}, obj);
+};
 
+export const removeDuplicatesBy = (keyFn, array) => {
+	/* return a copy of array of objects, where each item is unique based on the given key
+		 e.g. 
+		
+		 testArray = [
+			 {color: 'red', value: 32},
+			 {color: 'red', value: 15},
+			 {color: 'blue', value: 16},
+		]
+		
+		removeDuplicates(d => d.color, testArray) 
+
+		// returns 
+		[
+			 {color: 'red', value: 32},
+			 {color: 'blue', value: 16},
+		]
+		 https://stackoverflow.com/questions/32238602/javascript-remove-duplicates-of-objects-sharing-same-property-value
+	*/
+	let uniqueSet = new Set();
+	return array.filter((d) => {
+		let key = keyFn(d);
+		let isNew = !uniqueSet.has(key);
+		if (isNew) {
+			uniqueSet.add(key);
+		}
+		return isNew;
+	});
+};
+
+// shuffle array * https://stackoverflow.com/a/2450976/1293256
+export const shuffle = (array) => {
+	var currentIndex = array.length;
+	var temporaryValue, randomIndex;
+
+	// While there remain elements to shuffle...
+	while (0 !== currentIndex) {
+		// Pick a remaining element...
+		randomIndex = Math.floor(Math.random() * currentIndex);
+		currentIndex -= 1;
+
+		// And swap it with the current element.
+		temporaryValue = array[currentIndex];
+		array[currentIndex] = array[randomIndex];
+		array[randomIndex] = temporaryValue;
+	}
+
+	return array;
+};
+
+// === GENERAL MATH FUNCTIONS ====================================================
 // get rand Float value between min, max (exclusive)
 export const randBw = (min, max) => {
 	return Math.random() * (max - min) + min;
@@ -18,17 +80,6 @@ export const randFromArr = (arr) => {
 // map input value to new range
 export const remap = (val, inputMin, inputMax, outputMin, outputMax) => {
 	return (outputMax - outputMin) * ((val - inputMin) / (inputMax - inputMin)) + outputMin;
-};
-
-// LERP: linear interpolation
-export const lerp = (v0, v1, t) => {
-	return v0 * (1 - t) + v1 * t;
-};
-
-// cosine interpolate between val1, val2, at progress (range 0-1)
-export const cosineInterpolate = (val1, val2, progress) => {
-	const m2 = (1 - Math.cos(progress * Math.PI)) / 2;
-	return val1 * (1 - m2) + val2 * m2;
 };
 
 // clamp/constrain value to min/max range
@@ -66,6 +117,23 @@ export const polarToCart = (r, theta) => {
 	};
 };
 
+// LERP: linear interpolation
+export const lerp = (v0, v1, t) => {
+	return v0 * (1 - t) + v1 * t;
+};
+
+// cosine interpolate between val1, val2, at progress (range 0-1)
+export const cosineInterpolate = (val1, val2, progress) => {
+	const m2 = (1 - Math.cos(progress * Math.PI)) / 2;
+	return val1 * (1 - m2) + val2 * m2;
+};
+
+// easing functions
+export const easeInOutQuad = (t) => {
+	return t < 0.5 ? 2 * t * t : -1 + (4 - 2 * t) * t;
+};
+
+// === COLOR FUNCTIONS ============================================================
 // get random color
 export const getRandomColor = () => {
 	var letters = '0123456789ABCDEF';
@@ -79,92 +147,6 @@ export const getRandomColor = () => {
 // test if number is between range (inclusive)
 export const isBetween = (val, min, max) => {
 	return val >= min && val <= max;
-};
-
-// shuffle array * https://stackoverflow.com/a/2450976/1293256
-export const shuffle = (array) => {
-	var currentIndex = array.length;
-	var temporaryValue, randomIndex;
-
-	// While there remain elements to shuffle...
-	while (0 !== currentIndex) {
-		// Pick a remaining element...
-		randomIndex = Math.floor(Math.random() * currentIndex);
-		currentIndex -= 1;
-
-		// And swap it with the current element.
-		temporaryValue = array[currentIndex];
-		array[currentIndex] = array[randomIndex];
-		array[randomIndex] = temporaryValue;
-	}
-
-	return array;
-};
-
-export const formatCurrency = (value, decimalPts = 0) => {
-	// return string with appropriate suffix for currency (K, M, B, T, etc);
-	value = Math.round(value);
-	let nDigits = `${value}`.length;
-	let suffix = '';
-	if (nDigits > 3 && nDigits <= 6) {
-		suffix = 'K';
-		value = (value / 1_000).toFixed(decimalPts);
-	} else if (nDigits > 6 && nDigits <= 9) {
-		suffix = 'M';
-		value = (value / 1_000_000).toFixed(decimalPts);
-	} else if (nDigits > 9 && nDigits <= 12) {
-		suffix = 'B';
-		value = (value / 1_000_000_000).toFixed(decimalPts);
-	} else if (nDigits > 12 && nDigits <= 15) {
-		suffix = 'T';
-		value = (value / 1_000_000_000_000).toFixed(decimalPts);
-	}
-
-	return `$${value}${suffix}`;
-};
-
-export const formatOrdinal = (value) => {
-	const suffixes = ['th', 'st', 'nd', 'rd'];
-	const v = value % 100;
-	return value + (suffixes[(v - 20) % 10] || suffixes[v] || suffixes[0]);
-};
-
-export const capitalize = (word) => word.charAt(0).toUpperCase() + word.slice(1);
-
-export const degreeToCompass = (degrees) => {
-	const directions = ['N', 'NE', 'E', 'SE', 'S', 'SW', 'W', 'NW', 'N'];
-	const index = Math.round((degrees % 360) / 45);
-	return directions[index];
-};
-
-export const removeDuplicatesBy = (keyFn, array) => {
-	/* return a copy of array of objects, where each item is unique based on the given key
-		 e.g. 
-		
-		 testArray = [
-			 {color: 'red', value: 32},
-			 {color: 'red', value: 15},
-			 {color: 'blue', value: 16},
-		]
-		
-		removeDuplicates(d => d.color, testArray) 
-
-		// returns 
-		[
-			 {color: 'red', value: 32},
-			 {color: 'blue', value: 16},
-		]
-		 https://stackoverflow.com/questions/32238602/javascript-remove-duplicates-of-objects-sharing-same-property-value
-	*/
-	let uniqueSet = new Set();
-	return array.filter((d) => {
-		let key = keyFn(d);
-		let isNew = !uniqueSet.has(key);
-		if (isNew) {
-			uniqueSet.add(key);
-		}
-		return isNew;
-	});
 };
 
 export const hex2rgb = (hex) => {
@@ -221,4 +203,42 @@ export const rgbToHex = ([r, g, b]) => {
 
 	// Concatenate the hexadecimal values and return
 	return '#' + hexR + hexG + hexB;
+};
+
+// === FORMATTING FUNCTIONS ============================================================
+export const formatCurrency = (value, decimalPts = 0) => {
+	// return string with appropriate suffix for currency (K, M, B, T, etc);
+	value = Math.round(value);
+	let nDigits = `${value}`.length;
+	let suffix = '';
+	if (nDigits > 3 && nDigits <= 6) {
+		suffix = 'K';
+		value = (value / 1_000).toFixed(decimalPts);
+	} else if (nDigits > 6 && nDigits <= 9) {
+		suffix = 'M';
+		value = (value / 1_000_000).toFixed(decimalPts);
+	} else if (nDigits > 9 && nDigits <= 12) {
+		suffix = 'B';
+		value = (value / 1_000_000_000).toFixed(decimalPts);
+	} else if (nDigits > 12 && nDigits <= 15) {
+		suffix = 'T';
+		value = (value / 1_000_000_000_000).toFixed(decimalPts);
+	}
+
+	return `$${value}${suffix}`;
+};
+
+export const formatOrdinal = (value) => {
+	const suffixes = ['th', 'st', 'nd', 'rd'];
+	const v = value % 100;
+	return value + (suffixes[(v - 20) % 10] || suffixes[v] || suffixes[0]);
+};
+
+export const capitalize = (word) => word.charAt(0).toUpperCase() + word.slice(1);
+
+// === MISC FUNCTIONS ============================================================
+export const degreeToCompass = (degrees) => {
+	const directions = ['N', 'NE', 'E', 'SE', 'S', 'SW', 'W', 'NW', 'N'];
+	const index = Math.round((degrees % 360) / 45);
+	return directions[index];
 };
