23d25dcdc0f8a5afa6ec743e883a0ce560aed5bb
Jeff MacInnes <jeff.macinnes@gmail.com>
2025-05-19 21:07:28 -0700

    updated SEO component

---
+++ src/lib/components/Helpers/SEO.svelte
@@ -0,0 +1,28 @@
+<script>
+	export let title = 'My Site Title';
+	export let description = 'This is a description of my site.';
+	export let image = '/images/socialCard.jpg';
+	export let url = 'https://your-domain.com';
+</script>
+
+<svelte:head>
+	<title>{title}</title>
+	<meta name="description" content={description} />
+
+	<!-- Open Graph / Facebook -->
+	<meta property="og:type" content="website" />
+	<meta property="og:url" content={url} />
+	<meta property="og:title" content={title} />
+	<meta property="og:description" content={description} />
+	<meta property="og:image" content={image} />
+
+	<!-- Twitter -->
+	<meta property="twitter:card" content="summary_large_image" />
+	<meta property="twitter:url" content={url} />
+	<meta property="twitter:title" content={title} />
+	<meta property="twitter:description" content={description} />
+	<meta property="twitter:image" content={image} />
+
+	<!-- Canonical URL -->
+	<link rel="canonical" href={url} />
+</svelte:head>
